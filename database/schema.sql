e3ac11b3cc2f7d6164b13f33ddf22ce58cf07d12
arwadley <wadley.alyssa@gmail.com>
2019-11-30 18:18:13 -0600

    20 items added to db

--- database/schema.sql
+++ database/schema.sql
@@ -144,5 +144,96 @@ INSERT IGNORE INTO items(id, box_number, product_description, product_size, care
 'Environment\nThe material in this product MAY BE recyclable. Please check the recycling rules in your community and if recycling facilities exist in your area.
 \n\nMaterials\nFrame:\nSteel, Chrome plated\n\nArmrest cover:\nSynthetic rubber', 
 'DIETMAR\nUnderframe for armchair\n\nPackage(s): 1'); 
+INSERT IGNORE INTO items(id, box_number, product_description, product_size, care_instructions, environment_and_materials, package_details) VALUES(11,
+'003.308.16', "We know that children's skin is extra sensitive, but not to worry. \nThis product has been tested, approved and is completely free from substances that may harm your child's skin or health.
+\nRaised edge prevents spills from ending up on the floor.\nThe smooth surface has no nooks or corners which are difficult to reach, making the chair easy to clean.
+\n\nDesigner\nJ Fritzdorf/J Feldman/J Hedberg", 'Width: 17 3/8 "\nDepth: 16 1/2 "\nThickness: 1 5/8 "', 
+'Wipe clean with a mild soapy solution.\n\nWipe dry with a clean cloth.', 'Environment\nNo BPA (Bisphenol A) added.
+\n\nMaterials\nPolypropylene', 'LANGUR\nHigh chair tray\n\nPackage(s): 1'); 
+INSERT IGNORE INTO items(id, box_number, product_description, product_size, care_instructions, environment_and_materials, package_details) VALUES(12, 
+'502.111.04', 'Easy to bring home since the whole dining set is packed in one box.
+\n\nThe wood expression and design of the backrest bring a traditional look to the room.
+\n\nEnhance the comfort by adding soft chair pads that fit the seat perfectly.\n\nPine ages beautifully as the color deepens and becomes richer over time.
+\n\nDurable solid wood and antique color finish will outlast many years of use.
+\n\nWe have tested it for you! The table’s surface is resistant to liquids, food stains, oil, heat, scratches and bumps, while its construction is stable, strong and durable to withstand years of daily use.
+\n\nSeats 4.\n\nRecommended for indoor use only.\n\nFor increased stability, re-tighten the screws about two weeks after assembly and when necessary.
+\n\nDesigner\nCarina Bengs', 'Table length: 46 1/2 "\nTable width: 29 1/8 "\nTable height: 29 1/8 "\nSeat width: 16 1/8 "
+\nSeat depth: 16 1/8 "\nSeat height: 17 3/8 "', 'Wipe clean using a damp cloth and a mild cleaner.\nWipe dry with a clean cloth.', 
+'Environment\n100% renewable material. (Fixtures excluded)
+\n\nWe have clear requirements for all the wood we use, including a ban on illegally harvested wood. By 2020, we want all our wood to come from more sustainable sources, defined as certified or recycled.
+\n\nProduct possible to recycle or use for energy recovery, if available in your community.\n\nMaterials\nSolid pine, Stain, Clear acrylic lacquer',
+'JOKKMOKK\nTable and 4 chairs\n\nPackage(s): 1'); 
+INSERT IGNORE INTO items(id, box_number, product_description, product_size, care_instructions, environment_and_materials, package_details) VALUES(13,
+'002.110.88', 'Solid wood is a durable natural material.\n\nFor increased stability, re-tighten the screws about two weeks after assembly and when necessary.
+\n\nMay be completed with FIXA self-adhesive floor protectors to protect the underlying surface against wear.
+\n\nThis chair has been tested for home use and meets the requirements for durability and safety, set forth in the following standards: EN 12520 and EN 1022.
+\n\nDesigner\nIKEA of Sweden', 'Tested for: 243 lb\nWidth: 16 1/2 "\nDepth: 19 1/4 "\nHeight: 35 3/8 "\nSeat width: 14 1/8 "
+\nSeat depth: 15 3/8 "\nSeat height: 17 3/4 "', 'Wipe clean with a mild soapy solution.\n\nWipe dry with a clean cloth.
+\n\nFor maximum quality, re-tighten the screws when necessary.', 'Materials\nSolid pine, Stain, Clear acrylic lacquer', 
+'STEFAN\nChair\n\nPackage(s): 1'); 
+INSERT IGNORE INTO items(id, box_number, product_description, product_size, care_instructions, environment_and_materials, package_details) VALUES(14,
+'804.343.58', "Easy to assemble without tools or screws.\n\nYou sit comfortably thanks to the chair's shaped back and seat.
+\n\nDesigner\nCarl Öjerstam", 'Tested for: 220 lb\nWidth: 22 7/8 "\nDepth: 22 "\nHeight: 33 1/2 "\nSeat width: 17 3/8 "
+\nSeat depth: 17 3/8 "\nSeat height: 17 3/4 "', 'Do not wash.\n\nDo not bleach.\n\nDo not tumble dry.
+\n\nDo not iron.\n\nDo not dry clean.', 'Materials\nTotal composition:
+\n65 % polyester, 35 % cotton\n\nFoot:\nPolypropylene\n\nSeat frame:\nReinforced polypropylene\n\nSeat shell:
+\nPolyester fiber fill, Non-woven polyester fabric\n\nSeat cushion:\nPolypropylene, High resilient polyurethane foam (cold foam).
+\n\nLeg:\nSolid ash, Tinted clear acrylic lacquer', 'BALTSAR\nChair\n\nPackage(s): 1'); 
+INSERT IGNORE INTO items(id, box_number, product_description, product_size, care_instructions, environment_and_materials, package_details) VALUES(15,
+'704.531.25', 'You sit comfortably thanks to the high back and seat with polyester wadding.\n\nThe chair legs are made of solid wood, which is a durable natural material.
+\n\nMachine washable cover; easy to keep clean.\n\nFor increased stability, re-tighten the screws about two weeks after assembly and when necessary.
+\n\nMay be completed with FIXA stick-on floor protectors to protect the underlying surface against wear.\n\nDesigner\nIKEA of Sweden', 
+'Tested for: 243 lb\nWidth: 20 1/8 "\nDepth: 22 7/8 "\nHeight: 37 3/4 "\nSeat width: 20 1/8 "\nSeat depth: 15 3/4 "\n Seat height: 18 7/8 "', 
+'Frame\nWipe dry with a clean cloth.\n\nCover\nMachine wash warm, normal cycle.\nDo not bleach.
+\nDo not tumble dry.\nIron medium temperature, max 300°F/150°C.\nDo not dry clean.', 
+'Environment\nWe have clear requirements for all the wood we use, including a ban on illegally harvested wood. By 2020, we want all our wood to come from more sustainable sources, defined as certified or recycled.
+\n\nMaterials\nTotal composition/ Cover:\n100 % polyester\n\nFrame:\nSolid eucalyptus wood, Stain, Clear acrylic lacquer
+\n\nSeat:\nFiberboard, Polyurethane foam., Polyester wadding\n\nBack:\n100% recycled solid cardboard, Polyurethane foam., Polyester wadding
+\n\nLining:\nNon-woven polypropylene', 'HARRY\nChair\n\nPackage(s): 1'); 
+INSERT IGNORE INTO items(id, box_number, product_description, product_size, care_instructions, environment_and_materials, package_details) VALUES(16,
+'101.356.59', 'The stool can be stacked, so you can keep several on hand and store them in the same space as one.
+\n\nThis stool has been tested for home use and meets the requirements for durability and safety, set forth in the following standards: EN 12520 and EN 1022.
+\n\nDesigner\nOla Wihlborg', 'Tested for: 220 lb\nSeat diameter: 12 5/8 "\nWidth: 15 3/4 "\nSeat height: 17 3/4 "', 
+'Wipe clean with a mild soapy solution.\nWipe dry with a clean cloth.\nFor maximum quality, re-tighten the screws when necessary.', 
+'Materials\nSeat/ Foot:\nPolypropylene\n\nLeg:\nSteel, Epoxy powder coating', 'MARIUS\nStool\n\nPackage(s): 1'); 
+INSERT IGNORE INTO items(id, box_number, product_description, product_size, care_instructions, environment_and_materials, package_details) VALUES(17,
+'092.195.27', "You can stack the chairs, so they take less space when you're not using them.
+\n\nThe self-adjusting plastic feet adds stability to the chair.\n\nFor increased stability, re-tighten the screws about two weeks after assembly and when necessary.
+\n\nDesigner\nIKEA of Sweden", 'Tested for: 220 lb\nWidth: 19 1/4 "\nDepth: 20 1/2 "\nHeight: 33 7/8 "\nSeat width: 15 "
+\nSeat depth: 15 "\nSeat height: 17 3/4 "', 'Seat shell/chair frame\nWipe dry with a clean cloth.
+\n\nChair\nWipe clean using a damp cloth and a mild cleaner.', 'Materials\nSeat shell
+\nMolded layer-glued wood veneer, Laminate, Clear acrylic lacquer\n\nChair frame\nSteel, Epoxy/polyester powder coating', 
+'Packages: 2\n\nMARTIN\nseat shell\n\nMARTIN\nchair frame'); 
+INSERT IGNORE INTO items(id, box_number, product_description, product_size, care_instructions, environment_and_materials, package_details) VALUES(18,
+'603.882.58', '10-year Limited Warranty. Read about the terms in the Limited Warranty brochure.
+\n\nChanging positions between sitting and standing helps you move your body so you both feel and work better.
+\n\nGives an active sitting position, which improves your posture.\n\nThe leather ages beautifully and acquires a nice patina over time.
+\n\nComplies with California TB117-2013 flammability requirements.
+\n\nThis product has been tested for public use and meets the requirements for safety and durability set forth in the following standards: EN 16139 and ANSI/BIFMA X5.1.
+\n\nComplies with California TB117-2013 flammability requirements.\n\nDesigner\nP Süssmann/J Karlsson', 
+'Tested for: 243 lb\nSeat diameter: 15 3/8 "\nWidth: 26 "\nHeight: 31 1/2 "\nSeat width: 15 3/8 "
+\nSeat depth: 15 3/8 "\nMin. seat height: 26 "\nMax. seat height: 31 1/2 "', 'Wipe clean with a water dampened cloth.', 
+'Materials\nSeat:\nMolded eucalyptus plywood\n\nSeat foam:\nHigh resilient polyurethane foam (cold foam).
+\n\nCover, underside:\nNon-woven polypropylene
+\n\nMechanism/ Gas cylinder/ Foot ring/ Star base leg:\nSteel, Epoxy powder coating
+\n\nLeg connector:\nReinforced polyamide\n\nFeet:\nSynthetic rubber\n\nMain parts:
+\nDyed through top-grain leather with a treated, embossed and pigmented surface', 'TROLLBERGET\nSit/stand support\n\nPackage(s): 1'); 
+INSERT IGNORE INTO items(id, box_number, product_description, product_size, care_instructions, environment_and_materials, package_details) VALUES(19,
+'448.811.00', 'Protects flooring against wear and dirt.\n\nSuitable for most types of floors and flat-woven rugs. Not suitable for cork floors or heated wooden floors.
+\n\nIf the floor protector is left lying on the floor for a long period a vacuum may form and cause damage to your floor. Therefore, lift the floor protector at regular intervals for ventilation.
+\n\nDesigner\nIKEA of Sweden', 'Length: 39 3/8 "\nWidth: 47 1/4 "\nThickness: 1/8 "', 
+'Wipe clean with a soft cloth dampened with water and a mild dish detergent or soap, if necessary.\nWipe dry with a clean cloth.
+\nIf the floor protector is left lying on the floor for a long period a vacuum may form and cause damage to your floor. 
+\nTherefore, lift the floor protector at regular intervals for ventilation.', 
+'Environment\nProduct possible to recycle or use for energy recovery, if available in your community.
+\n\nMaterials\nPET plastic, Polyurethane plastic', 'KOLON\nFloor protector\n\nPackage(s): 1'); 
+INSERT IGNORE INTO items(id, box_number, product_description, product_size, care_instructions, environment_and_materials, package_details) VALUES(20,
+'903.078.40', 'Hook and loop fasteners keep the chair pad in place.\n\nThe chair cushion has two identical sides so it can be turned over for even wear.
+\n\nMachine washable for easy care.\n\nFits KAUSTBY and INGOLF chairs.\n\nDesigner\nIKEA of Sweden', 
+'Min. width: 14 "\nMax. width: 16 "\nDepth: 15 "\nThickness: 3 "\nFilling weight: 12 oz\nTotal weight: 16 oz', 
+'Machine wash warm, normal cycle.\nDo not bleach.\nTumble dry, low, normal cycle.\nDo not iron.\nDo not dry clean.', 
+'Environment\nRenewable material (cotton).\nTo lower our environmental impact this product is offered without any packaging.
+\n\nMaterials\nInterliner:\n100 % polypropylene\n\nFilling material:\n100 % polyester - hollow fiber (100% recycled)
+\n\nOuter fabric:\n100 % cotton', 'MALINDA\nChair pad\n\nPackage(s): 1'); 
 
 
