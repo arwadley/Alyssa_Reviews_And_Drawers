6453c1edf553b17b28eae05013b32d04f69e580b
arwadley <wadley.alyssa@gmail.com>
2019-12-02 14:00:19 -0600

    hover effect to change color  working on boxes. Modified CSS classes to eliminate duplicate code.

--- client/App.jsx
+++ client/App.jsx
@@ -4,6 +4,12 @@ import React from 'react';
 export default class App extends React.Component{
     constructor(props){
         super(props)
+
+        this.clickHandler= this.clickHandler.bind(this); 
+    }
+
+    clickHandler(){
+
     }
 
     render(){
@@ -37,7 +43,7 @@ export default class App extends React.Component{
               <span className='aw_drawer_label_styling aw_x_icon '>+</span>
               <span className='aw_drawer_label_styling  aw_drawer_label_text'>Reviews</span>
           </div> 
-          <div id='last_drawer'>
+          <div id='aw_last_drawer'>
               <span className='aw_drawer_label_styling aw_x_icon '>+</span>
               <span className='aw_drawer_label_styling  aw_drawer_label_text' >Product availability</span>
           </div> 
