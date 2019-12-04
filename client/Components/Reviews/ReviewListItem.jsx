0d774d00f890fe11c8cc3f06e9bbf3d975dc1a30
arwadley <wadley.alyssa@gmail.com>
2019-12-04 16:49:03 -0600

    stars populating for overall score; reviewer name displaying. Calculations for dates in progress to display how old the review is.

--- client/Components/Reviews/ReviewListItem.jsx
+++ client/Components/Reviews/ReviewListItem.jsx
@@ -2,16 +2,32 @@ import React from 'react';
 import StarRatingComponent from 'react-star-rating-component';
 
 const ReviewListItem = props => {
-  return (
-    <div>
-      <StarRatingComponent
-        name="overallRating"
-        editing={false}
-        value={props.review.overall_rating}
-        starCount={5}
-      />
-    </div>
-  );
+  const oneDay = 24 * 60 * 60 * 1000;
+  if (props.review) {
+    let postedDate = props.review.posted_date.split(/[- :T]/);
+    postedDate = new Date(
+      Number(postedDate[0]),
+      Number(postedDate[1] - 1),
+      Number(postedDate[2], 0, 0, 0)
+    );
+    let currentDate = new Date();
+    let difference = (currentDate - postedDate) / oneDay;
+    console.log(difference);
+
+    return (
+      <div>
+        <StarRatingComponent
+          name="overallRating"
+          editing={false}
+          value={props.review.overall_rating}
+          starCount={5}
+          emptyStarColor={'#E8E8E8'}
+        />
+        <span className="review_list_text">{props.review.user_name}</span>
+        <span>{}</span>
+      </div>
+    );
+  }
 };
 
 export default ReviewListItem;
